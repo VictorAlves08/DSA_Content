2bfb30d6fa4e686909ada0a8e5b8687fce8a9d9c
VictorAlves08 <victoralvees17@gmail.com>
2024-03-08 22:15:28 -0300

    remember diff between cin and cout

--- hello.cpp
+++ hello.cpp
@@ -4,6 +4,20 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World";
+    int KMsPerLiters;
+    double totalKMs, gasolineValue;
+
+    cout << "What is the distance of your trip?: " << endl;
+    cin >> totalKMs;
+
+    cout << "How many kilometers per liter?: " << endl;
+    cin >> KMsPerLiters;
+
+    cout << "What is the value of your gasoline?: " << endl;
+    cin >> gasolineValue;
+
+    double result = (totalKMs / KMsPerLiters) * gasolineValue;
+
+    cout << "Your trip will cost " << result << " dollars.";
     return 0;
 };
