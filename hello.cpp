bc1b730c39bcdaea48324f5a50ef979146d4a24b
VictorAlves08 <victoralvees17@gmail.com>
2024-03-09 15:43:31 -0300

    some information about system memory

--- hello.cpp
+++ hello.cpp
@@ -4,20 +4,19 @@ using namespace std;
 
 int main()
 {
-    int KMsPerLiters;
-    double totalKMs, gasolineValue;
-
-    cout << "What is the distance of your trip?: " << endl;
-    cin >> totalKMs;
-
-    cout << "How many kilometers per liter?: " << endl;
-    cin >> KMsPerLiters;
-
-    cout << "What is the value of your gasoline?: " << endl;
-    cin >> gasolineValue;
-
-    double result = (totalKMs / KMsPerLiters) * gasolineValue;
-
-    cout << "Your trip will cost " << result << " dollars.";
+    char justLetter = 'L';
+    string justWord = "Word ";
     return 0;
 };
+// g++ hello.cpp -o hello
+
+/*
+ - General Informations
+    BIT - Menor Unidade de Informação Digital (0 ou 1)
+    BYTE - 8 Bits -  Maior Decimal é 255 (11111111)
+
+    Baseado no tipo da variavel, o compilador irá pedir ao SO para alocar
+    um determinado espaço de memória (cédulas de memória). O propio SO decide quais cédulas serão utilizadas.
+
+    Eu declaro a variavel -> Compilador faz um pedido -> SO (Através de gerenciador de memoria) guarda em uma cedula em hexadecimal
+*/
